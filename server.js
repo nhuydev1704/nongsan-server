181bdc3188db9e12795cac9497f868005ab02100
nguyennhuy <khanhandli@gmail.com>
2022-01-18 21:45:31 +0700

    1

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(
     cors({
-        origin: `https://master--suspicious-meitner-3e7ec6.netlify.app`,
+        origin: '*',
         credentials: true,
     })
 );
