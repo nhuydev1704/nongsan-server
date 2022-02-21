62e9d971fd27e0e933245a230e6780cde25df78d
nguyennhuy1704 <khanhandli@gmail.com>
2022-02-21 22:46:00 +0700

    add domain

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 app.use(
     cors({
-        origin: 'http://localhost:3000',
+        origin: 'https://thuyhang.cf',
         credentials: true,
     })
 );
