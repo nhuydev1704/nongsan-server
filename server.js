31058a2224bf52ebecefca625b50e703369c672b
nguyennhuy1704 <y.nguyen@vinaseco.vn>
2022-04-08 22:57:18 +0700

    add paypal native

--- server.js
+++ server.js
@@ -6,23 +6,114 @@ const cors = require('cors');
 const path = require('path');
 const Comments = require('./models/commentModel');
 const AllRouter = require('./routes/index.js');
+const engines = require('consolidate');
+const paypal = require('paypal-rest-sdk');
+
 dotenv.config();
 
 // midleware
 const app = express();
+
+// engine and config paypal
+app.engine('ejs', engines.ejs);
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+paypal.configure({
+    mode: 'sandbox', //sandbox or live
+    client_id: 'AVqNO5CdRg3P1RJrzEzXa1xJoPsX8Eqe2bJt5WCHunVuIa_B7oB_GZjbLlHVX2yzPiji8qsw49nUJybO',
+    client_secret: 'EHQ2MxXRSkPPAZ_vmFoYUdwcP-bzSumHuvp1cmgEqj6rLB0MwL62R_fZqOKKPb9PIty-JWZ3SavRtB4W',
+});
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/paypal', (req, res) => {
+    var create_payment_json = {
+        intent: 'sale',
+        payer: {
+            payment_method: 'paypal',
+        },
+        redirect_urls: {
+            return_url: 'https://nongsan-app.herokuapp.com/success',
+            cancel_url: 'https://nongsan-app.herokuapp.com/cancel',
+        },
+        transactions: [
+            {
+                item_list: {
+                    items: [
+                        {
+                            name: 'item',
+                            sku: 'item',
+                            price: '1.00',
+                            currency: 'USD',
+                            quantity: 1,
+                        },
+                    ],
+                },
+                amount: {
+                    currency: 'USD',
+                    total: '1.00',
+                },
+                description: 'This is the payment description.',
+            },
+        ],
+    };
+
+    paypal.payment.create(create_payment_json, function (error, payment) {
+        if (error) {
+            throw error;
+        } else {
+            console.log('Create Payment Response');
+            console.log(payment);
+            res.redirect(payment.links[1].href);
+        }
+    });
+});
+
+app.get('/success', (req, res) => {
+    // res.send("Success");
+    var PayerID = req.query.PayerID;
+    var paymentId = req.query.paymentId;
+    var execute_payment_json = {
+        payer_id: PayerID,
+        transactions: [
+            {
+                amount: {
+                    currency: 'USD',
+                    total: '1.00',
+                },
+            },
+        ],
+    };
+
+    paypal.payment.execute(paymentId, execute_payment_json, function (error, payment) {
+        if (error) {
+            console.log(error.response);
+            throw error;
+        } else {
+            console.log('Get Payment Response');
+            console.log(JSON.stringify(payment));
+            res.render('success');
+        }
+    });
+});
+
+app.get('/cancel', (req, res) => {
+    res.render('cancel');
+});
+
+// Api
 app.use(
     cors({
-        origin: 'http://localhost:3000',
+        origin: 'https://thuyhang.cf',
         credentials: true,
     })
 );
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-app.get('/', (req, res) => {
-    res.json({ msg: 'Hello World' });
-});
 
 // Routes
 app.use('/api', AllRouter.auth);
